a5e387c2069f4d0a9d4e753d607a4d373350e477
Ilya Gusev <phoenixilya@gmail.com>
2019-06-15 22:38:11 +0300

    download.sh

---
+++ download.sh
@@ -0,0 +1,5 @@
+cd data
+wget https://www.dropbox.com/s/4stea3h10vbj3pc/ru_custom.txt
+wget https://www.dropbox.com/s/o5v3bcc5e3lvewq/zaliznyak.txt
+wget https://dumps.wikimedia.org/ruwiktionary/20190501/ruwiktionary-20190501-pages-articles.xml.bz2
+bzip2 -d ruwiktionary-20190501-pages-articles.xml.bz2
