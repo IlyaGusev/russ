95e8d3f9b2a409aa65907048e94fe79d11033aff
Ilya Gusev <phoenixilya@gmail.com>
2022-07-29 16:26:32 +0300

    fix

--- scripts/create_stress_dict.py
+++ scripts/create_stress_dict.py
@@ -1,20 +1,26 @@
 import argparse
 
 from russ.stress.dict import StressDict, Stress
-from russ.stress.reader import StressReader
+from russ.convert import convert_to_record
 
 
 def create_dict(files, output):
     stress_dict = StressDict()
-    reader = StressReader()
     for path in files:
         with open(path, "r", encoding="utf-8") as r:
             for line in r:
                 word = line.strip()
-                schema = list(map(int, reader.text_to_instance(word)["tags"].labels))
-                primary_stresses = [Stress(i-1, Stress.Type.PRIMARY) for i, stress in enumerate(schema) if stress == 1]
-                secondary_stresses = [Stress(i-1, Stress.Type.SECONDARY) for i, stress in enumerate(schema) if stress == 2]
-                clean_word = word.replace("`", "").replace("'", "")
+                record = convert_to_record(word)
+                schema = record["tags"]
+                clean_word = record["text"]
+                primary_stresses = [
+                    Stress(i - 1, Stress.Type.PRIMARY)
+                    for i, stress in enumerate(schema) if stress == 1
+                ]
+                secondary_stresses = [
+                    Stress(i - 1, Stress.Type.SECONDARY)
+                    for i, stress in enumerate(schema) if stress == 2
+                ]
                 stress_dict.update(clean_word, primary_stresses + secondary_stresses)
     stress_dict.save(output)
 
